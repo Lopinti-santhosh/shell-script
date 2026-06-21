086a14469e0fa8ec17438d629c7a389e19d8ff66
santhosh <smileysanthosh29@gmail.com>
2026-06-21 17:37:57 +0530

    first repo v2

--- 07.instalation.sh
+++ 07.instalation.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
-USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+#!/bin/bash
+
+
+userid=$(id -u)
+ 
+if [ $userid -eq 0 ]
 then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+   echo " you are not a root user"
+   exit 1
 else
-    echo "You are super user."
+echo " you are root user"
+
 fi
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]
-then
-    echo "Installation of mysql...FAILURE"
-    exit 1
-else
-    echo "Installation of mysql...SUCCESS"
+if [ $? -eq 0]
+
+then 
+   echo " sql is installed"
+else 
+   echo " sql is not installed"
+
 fi
-
-dnf install git -y
-
-if [ $? -ne 0 ]
-then
-    echo "Installation of git...FAILURE"
-    exit 1
-else
-    echo "Installation of Git...SUCCESS"
-fi
-
-echo "is script proceeding?"
