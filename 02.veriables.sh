395f164a206f016be3ff4cbb8faf60dbe52eaed4
santhosh <smileysanthosh29@gmail.com>
2026-06-21 14:50:22 +0530

    first repo v2

--- 02.veriables.sh
+++ 02.veriables.sh
@@ -7,5 +7,5 @@ person2=amani
 
 echo "$person1:: hi $person2 how are you"
 echo "$person2:: hi $person1 i am fine how are you"
-echo "$person1:: i am also fine"
+echo "$person1:: i am also fine,you "
 echo "$person2:: what are you doing? 
