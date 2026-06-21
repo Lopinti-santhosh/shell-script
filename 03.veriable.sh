f707bb5c32bd28d9b606682f5806f13798936d7e
santhosh <smileysanthosh29@gmail.com>
2026-06-21 14:59:48 +0530

    first repo v2

---
+++ 03.veriable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+
+
+echo "$person1:: hi $person2 how are you"
+echo "$person2:: hi $person1 i am fine how are you"
+echo "$person1:: i am also fine,what are you doing?"
+echo "$person2:: i am playing cricket,what about you?"
