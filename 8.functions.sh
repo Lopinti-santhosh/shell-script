9469d7326af81a2b266c08db193249ca5d1e6e48
santhosh <smileysanthosh29@gmail.com>
2026-06-22 10:34:37 +0530

    first repo v2

---
+++ 8.functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo " its failure"
+        exit 1
+    else
+        echo " its success"
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql -y
+
+validate $? "Installation of mysql"
+
+
+dnf install git -y
+
+validate $? "Installation of git"
+
+echo "is script proceeding?"
