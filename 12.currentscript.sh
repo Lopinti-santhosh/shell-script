1f71c56850fa71b75a4bef2297029fa90c417a33
santhosh <smileysanthosh29@gmail.com>
2026-06-28 14:23:45 +0530

    script

--- 12.currentscript.sh
+++ 12.currentscript.sh
@@ -5,7 +5,7 @@ course="deops from other script"
 echo "before calling other script , course name is $course"
 echo "before calling other script PID , $$ "
 
-#./13.otherscript.sh
+./13.otherscript.sh
 
 echo "after calling other script , course name is $course"
 echo "after calling other script PID , $$ "
---
+++ 13.otherscript.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+course="deops from other script"
+
+echo "after calling other script , course name is $course"
+echo "after calling other script PID , $$ "
